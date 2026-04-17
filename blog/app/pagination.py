76163f04b5e0278567d51708c8ce69640dd9bbc9
Shruti Ghumaliya <ghumaliyashruti2@gmail.com>
2026-04-17 14:26:55 +0530

    limitoffset and cursor pagination added

--- blog/app/pagination.py
+++ blog/app/pagination.py
@@ -1,7 +1,8 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination,LimitOffsetPagination,CursorPagination
 from rest_framework.response import Response
 
-class NumPagination(PageNumberPagination):
+'''class NumPagination(PageNumberPagination):
+
     page_size = 1
     page_size_query_param = 'page_size'
     max_page_size = 10
@@ -14,3 +15,26 @@ class NumPagination(PageNumberPagination):
             "previous": self.get_previous_link(),
             "data": data
         })
+'''
+'''        
+class NumPagination(LimitOffsetPagination):
+
+    # url .. ?limit=3&offset=2(here limit is number of record and offset start with that record number)
+    
+    default_limit = 2
+    # its show 5 record per page (its used when user not set limit and offset in your url )
+    limit_query_param = 'mylimit'
+    # instead of a name limit to mylimit (url .. ?mylimit=3&offset=2)
+    offset_query_param = 'myoffset'
+    # instead of name offset to myoffset (url .. ?mylimit=3&myoffset=2)
+    max_limit = 3
+    # show max 3 record per page . 
+'''
+
+class NumPagination(CursorPagination):
+    page_size = 2 # number of record show per page 
+    ordering = "is_private" # cursor pagination work based on data like id , name , date , etc ..
+    # here we want data base on user . 
+    cursor_query_param = "mycursor"
+    # instead name cursor to my cursor 
+    
